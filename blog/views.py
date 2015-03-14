cec25817cbf73246e7efeafb2bf23936e9ea1ad9
donny <chengxd@sina.cn>
2015-03-14 22:09:01 +0800

    First version.

---
+++ blog/views.py
@@ -0,0 +1,58 @@
+
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from .forms import PostForm
+
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
+
+from django.contrib.auth.decorators import login_required
+ 
+def post_list(request):
+    # posts = Post.objects.filter(published_date__isnull=False).order_by('-published_date') 
+    posts =Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+ 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+# def post_new(request):
+#     form = PostForm()
+#     return render(request, 'blog/post_edit.html', {'form': form})
+
+@login_required
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('blog.views.post_list')
